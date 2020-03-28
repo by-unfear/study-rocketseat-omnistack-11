f921743aa37f4a84fd7d19438d661ac67709f75b
Unfear <roberson@unfear.com.br>
2020-03-27 21:57:59 -0300

    inicio da aula de mobile

---
+++ mobile/src/pages/incidents/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, Image, View } from 'react-native';
+
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles';
+
+export default function Inciidents() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Image source={logoImg}/>
+				<Text style={styles.headerText}>
+					Total de<Text style={styles.headerTextBold}> 0 casos </Text> 
+				</Text>
+			</View>
+			<Text style={styles.title}>
+				Bem vindo
+			</Text>
+			<Text style={styles.description}>
+				Escolha um dos casos abaixo e salve o dia
+			</Text>
+		</View>
+	);
+}
+
